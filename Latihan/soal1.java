3367afec3cd1b8c23d87bd466d639392439964c4
2382033 <2382033@unai.edu>
2024-09-17 21:20:08 +0700

    Tugas 1

---
+++ Latihan/soal1.java
@@ -0,0 +1,14 @@
+package Latihan;
+
+public class soal1 {
+    public static void main(String[] args) {
+        int [] angka = {1, 2, 3};
+        int total = 0;
+        for (int i = 0; i <= 2; i++){
+
+            total += angka[i];
+
+        }
+        System.out.println(total);
+    }
+}
